a0211833c6675419fb31d58ad3c5fae585b59fd5
Emeya <emeya.iyaye@ust.edu.ng>
2023-02-16 05:28:38 -0800

    bit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  *main - prints the size of various types
- *   *Return: 0
- *    */
+ *  Main - prints the size of various types
+ *  Return: 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
