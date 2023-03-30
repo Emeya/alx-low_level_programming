a41bdeb1b595a610d4a0d861c50e7b60e0bf2e39
Emeya <122848222+Emeya@users.noreply.github.com>
2023-03-30 20:45:03 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,27 +1,40 @@
-#include <stdlib.h>
-#include <stdio.h>
+
+Gejix
+/
+alx-low_level_programming
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+alx-low_level_programming/0x12-singly_linked_lists/1-list_len.c
+@Gejix
+Gejix [feat]: Added file 1-list_len.c
+ 1 contributor
+23 lines (20 sloc)  306 Bytes
 #include "lists.h"
 
 /**
-  * print_list - Prints all elements of a list
-  * @h: A linked list
-  *
-  * Return: The number of nodes
-  */
-size_t print_list(const list_t *h)
-{
-size_t count = 0;
+ * list_len - prints number of elements in list_t list
+ *
+ * @h: singly linked list
+ *
+ * Return: number of elements in the list
+ *
+ */
 
+size_t list_len(const list_t *h)
+{
+size_t elements;
+
+elements = 0;
 while (h != NULL)
 {
-if (h->str == NULL)
-printf("[0] (nil)\n");
-else
-printf("[%d] %s\n", h->len, h->str);
-
 h = h->next;
-count++;
+elements++;
 }
-
-return (count);
+return (elements);
 }
